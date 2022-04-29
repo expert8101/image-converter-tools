b5f13329a98f2f705aa9bebdd650aa2bf39a9421
expert8101 <104355691+expert8101@users.noreply.github.com>
2022-04-29 23:36:57 +0500

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -23,10 +23,3 @@
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
-
-    <!-- =======================================================
-    * Template Name: Eterna - v4.7.0
-    * Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
-    * Author: BootstrapMade.com
-    * License: https://bootstrapmade.com/license/
-    ======================================================== -->
